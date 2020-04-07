4f40c78d33feb5b25a9cfe9307743a15a99afd38
sxd1040 <63058875+sxd1040@users.noreply.github.com>
2020-04-07 22:50:43 +0800

    线上修改一下代码

--- src/main/java/com/sxd/gitdemo/GitdemoApplication.java
+++ src/main/java/com/sxd/gitdemo/GitdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitdemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(GitdemoApplication.class, args);
     }
 
