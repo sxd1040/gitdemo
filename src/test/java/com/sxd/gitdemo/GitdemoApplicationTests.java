6fee8e1ecd69fce6cd54a130e3734ec119e20c92
sxd1040 <1040673479@qq.com>
2020-04-07 22:29:33 +0800

    第一次提交项目到github上去

---
+++ src/test/java/com/sxd/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxd.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
