27901c90e2ee4ab0b40781ff87aa9cea308c8664
sxd1040 <1040673479@qq.com>
2020-04-07 22:56:08 +0800

    修改了启动类

--- src/main/java/com/sxd/gitdemo/GitdemoApplication.java
+++ src/main/java/com/sxd/gitdemo/GitdemoApplication.java
@@ -8,6 +8,7 @@ public class GitdemoApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("错的不是我是这个世界");
         SpringApplication.run(GitdemoApplication.class, args);
     }
 
